43ee5176ae201adaa9c723555842d0cef60b3ff2
Brad Beltowski <bbelowski@fullsail.com>
2016-09-22 16:06:42 -0400

    Trying to debug

--- ng/controllers/GameCtrl.js
+++ ng/controllers/GameCtrl.js
@@ -2,14 +2,9 @@ angular.module('fireApp')
 .controller('GameCtrl', function($scope, $rootScope, $firebaseArray, currentAuth, PhaserSvc) {
 	var ref = firebase.database().ref()
 	var postRef = ref.child('users/' +currentAuth.uid+ "/game-collection" )
-	var collection = $firebaseArray(postRef) //prettycedezipink
+	var collection = $firebaseArray(postRef)
 
 	$scope.collection = collection
-
+	console.log("Game CTRL")
 	PhaserSvc.init()
-
-	$rootScope.$watch('score', function(newValue, oldValue) {
-	  console.log('HOLLA! ', newValue)
-	})
-
 })
